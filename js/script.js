88cebf78776c8e6e091ea5a7050152ada5113485
FiorellaQA <fiorella_90_17@hotmail.com>
2017-03-29 12:59:41 -0500

    añadiendo la funcionalidad de bienvenida

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 // OBJETO CONSTRUCTOR
-function usuario (nombre, apellido, correo, contraseña){
+function Usuario (nombre, apellido, correo, contraseña){
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.correo = correo;
 		this.contraseña = contraseña;	
 }
+
 function camposVacios(){
 	var indicacionesGenerales = document.getElementById("indicaciones-generales");
 
@@ -34,36 +35,72 @@ document.getElementById("bienvenida-registrar").addEventListener("click",functio
 document.getElementById("registrar").addEventListener("click",function(e){
 	e.preventDefault();
 
-	var nombre = document.getElementById("nombre");
-	var apellido = document.getElementById("apellido");
-	var correo = document.getElementById("correo");
-	var contraseña = document.getElementById("contraseña");
-	var array = document.getElementsByClassName("input-registro");
+	var nombre = document.getElementById("nombre").value;
+	var apellido = document.getElementById("apellido").value;
+	var correo = document.getElementById("correo").value;
+	var contraseña = document.getElementById("contraseña").value;
+	
 	var indicacionesGenerales = document.getElementById("indicaciones-generales");
 
-	//Aplicando la funcion camposVacios a todos los input del formulario
-	for(i = 0 ; i < array.length; i++){
-		array[i].addEventListener("blur", camposVacios);
-	}
 	//Validando que todos los campos esten llenos
-	if(nombre.value.length !=0 && apellido.value.length != 0 && correo.value.length != 0 && contraseña.value.length != 0){
+	if(nombre.length !=0 && apellido.length != 0 && correo.length != 0 && contraseña.length != 0){
 		var nuevoUsuario = new Usuario(nombre, apellido, correo, contraseña);
+		localStorage.setItem("data",JSON.stringify(nuevoUsuario));
+		document.getElementById("form-registro").style.display = "none";
+		document.getElementById("print-usuario").style.display = "block";
+		printUsuario();
 
 	}else{
 		indicacionesGenerales.innerHTML = "*Todos los campos son obligatorios.</br>";
 	}	
 });
+
+//Aplicando la funcion camposVacios a todos los input del formulario
+var array = document.getElementsByClassName("input-registro");
+	for(i = 0 ; i < array.length; i++){
+		array[i].addEventListener("blur", camposVacios);
+	}
 //validando correo
 var correo = document.getElementById("correo");
 var errorEmail = document.getElementById("messageEmail");
-	correo.onkeypress= function (){
+	correo.onblur= function (){
 		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     	if( ! (re.test(correo.value))){
     		errorEmail.innerHTML = "<br>Verificar que el email tenga un formato válido. Ej: name@domain.com";
     		correo.focus();
+    		return false;
+    	}else{
+    		return true;
     	}
 	};
 
+// Validando password
+var contraseña = document.getElementById("contraseña");
+var errorContraseña = document.getElementById("messageContraseña");
+contraseña.onblur = function(){
+	if(! (contraseña.value.length <=20 && contraseña.value.length >=6)){
+		errorContraseña.innerHTML = "<br> La contraseña debe tener entre 6 y 20 caracteres.";
+		contraseña.focus();
+		return false;
+	}else{
+		return true;
+	}
+};
+function printUsuario(){
+	var nombre = document.getElementById("nombre-usuario");
+	var apellido = document.getElementById("apellido-usuario");
+	var correo = document.getElementById("correo-usuario");
+	var contraseña = document.getElementById("contraseña-usuario");
+	var objetoUsuario = JSON.parse(localStorage.getItem("data"));
+	//Imprimimos en el <span>
+	nombre.innerHTML = objetoUsuario.nombre;
+	apellido.innerHTML = objetoUsuario.apellido;
+	correo.innerHTML = objetoUsuario.correo;
+	contraseña.innerHTML = objetoUsuario.contraseña;
+}
+document.getElementById("bt-edit").addEventListener("click",function(e){
+	alert("editando");
+});
 document.getElementById("bienvenida-iniciarSesion").addEventListener("click",function(e){
 	alert("chau");
 });
